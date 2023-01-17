c945569f3fa90ded7222a4f817c96173b41eb959
adi <1thayyiladi@hdsb.ca>
2023-01-17 08:58:42 -0500

    update vars

--- temp/temp.ino
+++ temp/temp.ino
@@ -15,9 +15,9 @@ float lightIntestityCalculations() {
   float prSensorValue = analogRead(prSensorPin);
 
   
-  float VOUT = float(prSensorValue) * (VIN / float(1024));  // Convert ADC value to volts
-  float RLDR = (prResistance * (VIN - VOUT))/VOUT;         // Convert from volts to resistance
-  float lux = 500/(RLDR/1000);                            // Convert resitance to lumen
+  float VOUT_LUM = float(prSensorValue) * (VIN / float(1023));  // Convert ADC value to volts
+  float RLDR = (prResistance * (VIN - VOUT_LUM)) / VOUT_LUM;   // Convert from volts to resistance
+  float lux = 500 / (RLDR / 1000);                            // Convert resitance to lumen
 
   return lux;
 }
@@ -26,9 +26,9 @@ float lightIntestityCalculations() {
 float tempCalculations() {
   float tempSensorValue = analogRead(tempSensorPin);
  
-  float VOUT_TEMP = tempSensorValue * VIN / float(1024) ;   // Convert ADC value to volts
-  float celsius = volts / .02 ;                            // Convert from Volts to Celsius (20mV per degree)
-  float fahrenheit = (celsius  * 9.0 / 5.0) + 32;         // Convert from Celsius to Fahrenheit
+  float VOUT_TEMP = float(tempSensorValue) * VIN / float(1023) ;   // Convert ADC value to volts
+  float celsius = VOUT_TEMP / .02 ;                               // Convert from Volts to Celsius (20mV per degree)
+  float fahrenheit = (celsius  * 9.0 / 5.0) + 32;                // Convert from Celsius to Fahrenheit
 
   return fahrenheit;
 }
