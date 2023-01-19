1f6d88b88ceab2c2583551ce2055ca21337fbb3b
adi <1thayyiladi@hdsb.ca>
2023-01-19 16:48:42 -0500

    added oled display

--- temp/temp.ino
+++ temp/temp.ino
@@ -1,5 +1,16 @@
-#define prResistance 10000
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
 #define VIN 5
+#define prResistance 10000
+
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+#define OLED_RESET -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C // I2C Address 0x3c
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 const int tempSensorPin = A0;
 const int prSensorPin = A1;
@@ -10,8 +21,19 @@ float temperature;
 float lightIntensity;
 float windSpeed;
 
+
 void setup() {
   Serial.begin(9600);
+  
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+  
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+
 }
 
 float calculateWindSpeed() {
@@ -53,6 +75,44 @@ void loop() {
   Serial.println("Temp: " + String(temperature) + " F");
   Serial.println("Light Intensity: " + String(lightIntensity) + " Lumens");
   Serial.println("Wind Speed: " + String(windSpeed) + " m/s");
+
+  display.clearDisplay();  // Updates Display
+
+  // Displays Title
+  display.setTextSize(1.6);
+  display.setCursor(0, 0);
+  display.println("All-in-One Sensor");
+  
+  // Display Temperature
+  display.setTextSize(1);
+  display.setCursor(0,20);
+  display.print("TMP: ");
+  display.setTextSize(1);
+  display.print(temperature);
+  display.print(" ");
+  display.setTextSize(1);
+  display.cp437(true);
+  display.write(167);
+  display.setTextSize(1);
+  display.print("F");
+  
+  // Display Light Intensity
+  display.setTextSize(1);
+  display.setCursor(0, 35);
+  display.print("LUM: ");
+  display.setTextSize(1);
+  display.print(lightIntensity);
+  display.print(" Lumens"); 
+
+  // Display Light Intensity
+  display.setTextSize(1);
+  display.setCursor(0, 50);
+  display.print("WIND: ");
+  display.setTextSize(1);
+  display.print(windSpeed);
+  display.print(" m/s"); 
+  
+  display.display(); 
 
   delay(1000);
 }
